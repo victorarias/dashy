b9a7798d1dc88ec1363858c28449922aa2b862a3
Victor Arias <arias.sp@gmail.com>
2014-08-19 23:14:39 +0200

    Add more pending tests

---
+++ test/server_test.js
@@ -0,0 +1,6 @@
+describe("App Server", function() {
+  describe("GET /data/:key", function() {
+    it("returns data associated with the key");
+    it("restricts access to data associated to the profile")
+  });
+});
